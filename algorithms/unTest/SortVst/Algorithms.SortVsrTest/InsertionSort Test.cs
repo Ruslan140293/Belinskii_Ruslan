669f8a828ec47ed707cad08295358f8f6e1abe6f
Ruslan140293 <ruslan140293@ukr.net>
2014-05-16 15:33:42 +0300

    no message

---
+++ algorithms/unTest/SortVst/Algorithms.SortVsrTest/InsertionSort Test.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Linq;
+using NUnit.Framework;
+using SortVst;
+
+namespace Algorithms.SortVsrTest
+{
+    [TestFixture]
+    public class InsertionSortTest
+    {
+        private int[] GetNotSortedArray
+        {
+            get
+            {
+                var random = new Random();
+                return Enumerable.Range(1, 10).OrderBy(r => random.Next()).ToArray();
+            }
+        }
+
+        [Test]
+        public void InsertionSortBase_NotSortedArray_SortedArrayByAscending()
+        {
+            //Arrange
+            var notSortedArray = GetNotSortedArray;
+            var sortedArrayByAscending = notSortedArray.OrderBy(x => x);
+            //Act
+            var resultArray = InsertionSort.Sort(notSortedArray);
+            //Assert
+            Assert.AreEqual(resultArray, sortedArrayByAscending);
+        }
+
+        [Test]
+        public void InsertionSortWithTrinar_NotSortedArray_SortedArrayByAscending()
+        {
+            //Arrange
+            var notSortedArray = GetNotSortedArray;
+            var sortedArrayByAscending = notSortedArray.OrderBy(x => x);
+            //Act
+            var resultArray = InsertionSort.SortTrinar(notSortedArray, true);
+            //Assert
+            Assert.AreEqual(resultArray, sortedArrayByAscending);
+        }
+
+        [Test]
+        public void InsertionSortWithTrinar_NotSortedArray_SortedArrayByDecending()
+        {
+            //Arrange
+            var notSortedArray = GetNotSortedArray;
+            var sortedArrayByDecending = notSortedArray.OrderByDescending(x => x);
+            //Act
+            var resultArray = InsertionSort.SortTrinar(notSortedArray, false);
+            //Assert
+            Assert.AreEqual(resultArray, sortedArrayByDecending);
+        }
+
+        [Test]
+        public void InsertionSortWithDelegate_NotSortedArray_SortedArrayByAscending()
+        {
+            //Arrange
+            var notSortedArray = GetNotSortedArray;
+            var sortedArrayByAscending = notSortedArray.OrderBy(x => x);
+            //Act
+            var resultArray = InsertionSort.SortWithDelegate(notSortedArray, (x, y) => x > y);
+            //Assert
+            Assert.AreEqual(resultArray, sortedArrayByAscending);
+        }
+
+        [Test]
+        public void InsertionSortWithDelegate_NotSortedArray_SortedArrayByDescending()
+        {
+            //Arrange
+            var notSortedArray = GetNotSortedArray;
+            var sortedArrayByDescending = notSortedArray.OrderByDescending(x => x);
+            //Act
+            var resultArray = InsertionSort.SortWithDelegate(notSortedArray, (x, y) => x < y);
+            //Assert
+            Assert.AreEqual(resultArray, sortedArrayByDescending);
+        }
+    }
+}
